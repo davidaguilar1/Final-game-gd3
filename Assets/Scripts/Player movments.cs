f4be5925deb93b6bbad8690a5d9b04c1e9a4c79c
davidaguilar1 <100818462+davidaguilar1@users.noreply.github.com>
2025-11-12 15:17:20 -0500

    1.0.1

--- Assets/Scripts/Player movments.cs
+++ Assets/Scripts/Player movments.cs
@@ -6,8 +6,9 @@ public class Playermovments : MonoBehaviour
     public GameObject LeftLane;
     public GameObject RightLane;
     public GameObject Middlelane;
+    public GameObject Note;
     public float jumppower = 3;
-
+    
     public float playerpos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,10 +17,24 @@ public class Playermovments : MonoBehaviour
 
     }
 
+    public void OnCollisionEnter(Collision collision)
+    {
+             if (gameObject.CompareTag("Note"))
+             {
+            Debug.Log("attack");
+                Destroy(Note);
+             }
+        
+
+    }
     // Update is called once per frame
     void Update()
     {
-       
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position += Vector3.up * jumppower * Time.deltaTime;
+        }
         if (Input.GetKeyDown(KeyCode.A))
         {
             transform.position = LeftLane.transform.position;
@@ -33,6 +48,8 @@ public class Playermovments : MonoBehaviour
         {
             transform.position = RightLane.transform.position;
         }
+
+       
     }
 
 }
