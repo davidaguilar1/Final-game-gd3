acf09f8f07c6022c8af6192d44492c05c91708fc
davidaguilar1 <100818462+davidaguilar1@users.noreply.github.com>
2025-11-19 15:29:36 -0500

    1.0.3

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    public TMP_Text Healthy;
+    public int Points;
+    public GameObject note;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Healthy.text = "Boss Health: 100" + Mathf.Round(Points).ToString();
+    }
+}
--- Assets/Scripts/Note cript.cs
+++ Assets/Scripts/Note cript.cs
@@ -1,30 +1,35 @@
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class Notecript : MonoBehaviour
 {
-    
+
+    public TMP_Text Health;
+    public float Points = 100;
     public float Movemnetspeed = 5;
+    public bool IsDestroyed = false;
+    public Health health;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        health= FindFirstObjectByType<Health>();
     }
 
+   
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.back * Movemnetspeed * Time.deltaTime;
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (gameObject.CompareTag("Note"))
         {
-          Destroy(gameObject);
+            IsDestroyed = true;
+            Destroy(gameObject);
+            health.Points -= 1;
         }
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
-        {
-
-        }
-        transform.position += Vector3.back * Movemnetspeed * Time.deltaTime;
     }
 }
